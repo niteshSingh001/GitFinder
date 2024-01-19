419f6b6db5eea2c5d928ddbae7b3f543f48daa76
Nitesh Singh <ns368497@gmail.com>
2024-01-19 17:27:40 +0530

    successfully added user profile data

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
-function fetchUser() {
-  const userName = document.querySelector("#userName").value;
-  fetchProfile(userName);
-}
+const resultContainer = document.getElementById("resultContainer");
 
-async function fetchProfile(userName) {
-  try {
-    const apiURL = `https://api.github.com/users/${userName}`;
-
-    document.getElementById("loading").style.display = "block";
-
-    const response = await fetch(apiURL);
-
-    if (!response.ok) {
-      alert("Enter correct username");
-      throw new Error("Network response was not ok");
-    } else {
-      const userData = await response.json();
-      document.getElementById("loading").style.display = "none";
-      setUserData(userData);
-
-      console.log("API RESPONSE", userData);
-    }
-  } catch (error) {
-    document.getElementById("loading").style.display = "none";
-    console.error("Fetch error:", error);
+function handleEnter(event) {
+  if (event.key === "Enter") {
+    searchUser();
   }
 }
 
-function setUserData() {}
+async function searchUser() {
+  const username = document.getElementById("usernameInput").value.trim();
+  const loader = document.getElementById("loader");
+  // const paginationContainer = document.getElementById('pagination');
+
+  if (username === "") {
+    alert("Please enter a valid username.");
+    return;
+  }
+
+  try {
+    loader.style.display = "block";
+
+    const userData = await getUserData(username);
+    const repoData = await getUserRepos(username);
+
+    console.log("USER PROFILE", userData);
+    console.log("USER REPO", repoData);
+    displayUserProfile(userData);
+
+    // Display pagination
+    // displayPagination(repoData.length, paginationContainer);
+  } catch (error) {
+    alert("Username not found.");
+  } finally {
+    loader.style.display = "none";
+  }
+}
+
+async function getUserData(username) {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  if (!response.ok) {
+    throw new Error("User not found.");
+  }
+
+  return await response.json();
+}
+
+async function getUserRepos(username) {
+  const response = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=created&per_page=10`
+  );
+  if (!response.ok) {
+    throw new Error("Error fetching repositories.");
+  }
+
+  return await response.json();
+}
+
+function displayUserProfile(userData) {
+  const imgContainer = document.getElementById("imgContainer");
+  const bioContainer = document.getElementById("bioContainer");
+
+  imgContainer.innerHTML = `
+    <img src=${userData.avatar_url} alt="userImg">
+    <a href=${userData.html_url}><i class="fa-solid fa-link"></i> ${userData.html_url}</a>
+    `;
+
+  bioContainer.innerHTML = `<h3>${userData.name}</h3>
+    <p>${userData.bio}</p>
+    <p><i class="fa-solid fa-location-dot"></i> ${userData.location}</p>
+    <p>Twitter:${userData.twitter_username}</p>`;
+}
